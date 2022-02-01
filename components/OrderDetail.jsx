e362658e69ec55f7d936b418c66c39e77fbdda51
Nikhil Nagrale <nknagrale634@gmail.com>
2022-02-01 20:42:46 +0530

    papyal payment and cash payment integration

---
+++ components/OrderDetail.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import styles from "../styles/OrderDetail.module.css";
+
+const OrderDetail = ({ total, createOrder }) => {
+  const [customer, setCustomer] = useState("");
+  const [address, setAddress] = useState("");
+
+  const handleClick = () => {
+    createOrder({ customer, address, total, method: 0 });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.title}>You will pay ₹50 after delivery.</h1>
+        <div className={styles.item}>
+          <label className={styles.label}>Name Surname</label>
+          <input
+            placeholder="Nikhil Nagrale"
+            type="text"
+            className={styles.input}
+            onChange={(e) => setCustomer(e.target.value)}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Phone Number</label>
+          <input
+            type="text"
+            placeholder="+91 974 5697 897"
+            className={styles.input}
+          />
+        </div>
+        <div className={styles.item}>
+          <label className={styles.label}>Address</label>
+          <textarea
+            rows={5}
+            placeholder="Subhash Nagar, Nagpur, Maharashtra"
+            type="text"
+            className={styles.textarea}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+        </div>
+        <button className={styles.button} onClick={handleClick}>
+          Order
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default OrderDetail;
